ec1b939d2352f9473f208e374668d8fcaec54587
Aline Tavares <alaisgomes@gmail.com>
2021-02-22 15:09:54 -0500

    Add support for connect endpoints - to be used by developers

--- backend/modules/social-auth/views.py
+++ backend/modules/social-auth/views.py
@@ -4,8 +4,8 @@ from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.providers.apple.views import AppleOAuth2Adapter
 from allauth.socialaccount.providers.apple.client import AppleOAuth2Client
-from rest_auth.registration.views import SocialLoginView
-from .serializers import CustomAppleSocialLoginSerializer
+from rest_auth.registration.views import SocialLoginView, SocialConnectView
+from .serializers import CustomAppleSocialLoginSerializer, CustomAppleConnectSerializer
 
 
 class FacebookLogin(SocialLoginView):
@@ -21,6 +21,22 @@ class GoogleLogin(SocialLoginView):
 
 class AppleLogin(SocialLoginView):
     adapter_class = AppleOAuth2Adapter
-    callback_url = 'https://anycallbackurlhere'
     client_class = AppleOAuth2Client
     serializer_class = CustomAppleSocialLoginSerializer
+
+
+class FacebookConnect(SocialConnectView):
+    permission_classes = (AllowAny,)
+    adapter_class = FacebookOAuth2Adapter
+
+
+class GoogleConnect(SocialConnectView):
+    permission_classes = (AllowAny,)
+    adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
+
+
+class AppleConnect(SocialConnectView):
+    adapter_class = AppleOAuth2Adapter
+    client_class = AppleOAuth2Client
+    serializer_class = CustomAppleConnectSerializer
